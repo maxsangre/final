e5ff736685d9f1ab748997599b30a7aa967f3cb2
maxsangre <max_sangre@live.com.ar>
2023-11-16 17:33:15 -0300

    3.0

---
+++ final/src/test/java/finalmx/demo/FinalApplicationTests.java
@@ -0,0 +1,16 @@
+package finalmx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
